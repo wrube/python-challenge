a9c8de5abb1c781ec452d3d8ea069db7f6ce97a9
wrube <bjminwruck@gmail.com>
2023-12-08 12:26:16 +1030

    added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -46,26 +46,25 @@ for month in range(numMonths):
 # need to set the first month difference to 0 as we don't have the previous months info
 monthly_diff[0] = 0
 
-
-
-total_profit_loss = 0
+# calculate changes in profit
 total_change_in_profit_loss = 0
-for i in range(numMonths):
-    total_profit_loss += profit_loss[i]
-    total_change_in_profit_loss += monthly_diff[i]
+for month in range(numMonths):
+    total_change_in_profit_loss += monthly_diff[month]
+
+# calculate the average change in profit/loss   
 avg_change_profit_loss = total_change_in_profit_loss / (numMonths-1)
 
-
+# calculate which month and quantity of the greatest increase in profit
 max_profit_index = monthly_diff.index(max(monthly_diff))
 max_profit_month = dates[max_profit_index]
 max_profit_amt = monthly_diff[max_profit_index]
 
-
+# calculate which month and quantity of the greatest decrease in profit
 min_profit_index = monthly_diff.index(min(monthly_diff))
 min_profit_month = dates[min_profit_index]
 min_profit_amt = monthly_diff[min_profit_index]
-# print(min_profit_month, min_profit_amt)
 
+# output results into console and file
 output_text = \
 f"""Financial Analysis
 -----------------------------------
@@ -75,15 +74,8 @@ Average change: ${avg_change_profit_loss:.2f}
 Greatest Increase in Profits: {max_profit_month} (${int(max_profit_amt)})
 Greatest Decrease in Profits: {min_profit_month} (${int(min_profit_amt)})
 """
-
-
 print(output_text)
-
 
 output_file = working_dir / "pybank_output.txt"
 with output_file.open(mode="w", encoding="utf-8") as file:
     file.write(output_text)
-
-
-
-
